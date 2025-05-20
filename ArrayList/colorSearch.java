7e33f39bc641f79a1da6f8d618d10156d702606a
Varhitha R <varshitharcse@gmail.com>
2025-05-20 14:24:42 +0530

    Initial files

--- ArrayList/colorSearch.java
+++ ArrayList/colorSearch.java
@@ -1,3 +1,7 @@
+/*Write a java program for getting different colors through ArrayList interface and search whether
+the color "Red" is available or not */
+
+
 package ArrayList;
 
 import java.util.ArrayList;
